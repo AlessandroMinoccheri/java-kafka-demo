a12a3df0a1e729034bc6ba01aa337cbfc83b5392
AlessandroMinoccheri <aminoccheri@inpost.it>
2023-08-19 07:22:47 +0200

    added unit test

---
+++ src/test/java/com/minompi/java_kafka_example/service/SendMessageTests.java
@@ -0,0 +1,35 @@
+package com.minompi.java_kafka_example.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+
+import com.minompi.java_kafka_example.MessageRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class SendMessageTests {
+    @Mock
+    KafkaTemplate<String, String> kafkaTemplate;
+
+    @BeforeEach
+    public void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("1 + 1 = 2")
+    void sendMessageCorrectly() {
+        MessageRequest messageRequest = new MessageRequest("Foo Bar Baz");
+        SendMessage sendMessage = new SendMessage(kafkaTemplate);
+
+        sendMessage.execute(messageRequest);
+        Mockito.verify(kafkaTemplate, times(1)).send("minompi", messageRequest.message());
+    }
+
+}
+    
